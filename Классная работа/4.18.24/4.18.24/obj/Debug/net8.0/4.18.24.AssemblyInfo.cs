2a4c02f609d6f30f0a46f907f9706080bed19c0b
MellSofia <sofaahmutdinova@gmail.com>
2024-04-19 23:47:02 +0400

    -

--- Классная работа/4.18.24/4.18.24/obj/Debug/net8.0/4.18.24.AssemblyInfo.cs
+++ Классная работа/4.18.24/4.18.24/obj/Debug/net8.0/4.18.24.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4.18.24")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc79b27eb071a672eda9f108cf7689367d29661e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3fa46d927a97f65657c9865477ab85061b87b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("4.18.24")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4.18.24")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
