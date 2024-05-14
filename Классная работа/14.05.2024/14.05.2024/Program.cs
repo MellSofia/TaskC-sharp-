d94fa7568edc035e7223abf870c1466466d5f1bf
MellSofia <sofaahmutdinova@gmail.com>
2024-05-14 19:23:50 +0400

    14.05

---
+++ Классная работа/14.05.2024/14.05.2024/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using prog;
+
+class HelloWord 
+{ 
+    static void Main()
+    {
+        Console.WriteLine();
+
+        Car c = new Car(3, "light");
+        c.Collision += (Car cur_car) => { Console.WriteLine(cur_car+"hitted"); };
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            c.update(rnd.Next(1, 5));
+        }
+    }
+}
+
