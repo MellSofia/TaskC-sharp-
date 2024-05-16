0d8b4d9f774d99e6652d3b7207c66068b2b9de75
MellSofia <sofaahmutdinova@gmail.com>
2024-05-16 19:19:28 +0400

    16.05.2024

---
+++ Классная работа/16,05,2024/16,05,2024/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using school;
+using System.Data.SqlTypes;
+using System;
+
+class Program {
+    static void Main()
+    {
+            List<Student> students = new List<Student> {
+            new Student("Mark", 3),
+            new Student("Alex", 5),
+            new Student("Max", 4),
+            new Student("Anatoly", 3),
+            new Student("Hurry", 5)
+        };
+
+            IEnumerable<Student> queryH = from s in students
+                                      where (s.mark > 4) && (s.went = false)
+                                      orderby s.mark descending
+                                      select s;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine($"day {i + 1}");
+
+            if (queryH.Count() > 0) { }
+        }
+
+    }
+} 
---
+++ Классная работа/16,05,2024/16,05,2024/school.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school
+{
+    class Student
+    {
+        public int mark;
+        public string Name { get; set; }
+        public bool went {  get; set; }
+        public bool punished {  get; set; }
+
+        public Student(string name, int mark1)
+        {
+            Name = name;
+            went = false;
+            punished = false;
+            mark=mark1;
+        }
+    }
+
+}
