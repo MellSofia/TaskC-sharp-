57b70bf191772542873192f23e6344719bde69ac
MellSofia <sofaahmutdinova@gmail.com>
2024-05-19 13:54:22 +0400

    dz 17.05

---
+++ DZ/dz_on_17.05.2024/14.05.2024/14.05.2024/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace prog
+{
+    class Car
+    {
+        public delegate void handler(Car c);
+        public event handler Collision;
+        public event Action Destroyed;
+
+        string name { get; set; }
+        public int Life { get; set; }
+
+        public Car(int life, string n)
+        {
+            Life = life;
+            name = n;
+        }
+        public void update(int a)
+        {
+            if (a == 3) // Урон наносится, если переданное значение равно 3
+            {
+                Life--;
+                Collision?.Invoke(this);
+            }
+            if (Life < 1) // Если у машины закончилась жизнь
+            {
+                Destroyed?.Invoke();
+            }
+        }
+        public override string ToString() { return name; }
+    }
+}
---
+++ DZ/dz_on_17.05.2024/14.05.2024/14.05.2024/Program.cs
@@ -0,0 +1,30 @@
+﻿using prog;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var car = new Car(3, "Car1"); // Создаем машину с 3 жизнями
+        car.Collision += Car_Collision;
+        car.Destroyed += Car_Destroyed;
+
+        var rnd = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            car.update(rnd.Next(1, 6));
+            Console.Write("-");
+            if (car.Life < 1) break; // Если у машины закончилась жизнь, завершаем цикл
+        }
+    }
+
+    private static void Car_Destroyed()
+    {
+        Console.WriteLine("\nМашина уничтожена.");
+    }
+
+    private static void Car_Collision(Car c)
+    {
+        Console.WriteLine($"\nМашина {c} получила урон.");
+    }
+}
