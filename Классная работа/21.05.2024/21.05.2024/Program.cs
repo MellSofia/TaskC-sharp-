5571dbd7e66bd553407f2ac2de7679a675d2f2c5
MellSofia <sofaahmutdinova@gmail.com>
2024-05-21 19:25:33 +0400

    21.05.2024

---
+++ Классная работа/21.05.2024/21.05.2024/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class prog{
+    static void Main(string[] args)
+    {
+        using (FileStream fileStream = new FileStream("TEMPFILE.txt", FileMode.Create))
+        {
+            using (StreamWriter writer = new StreamWriter(fileStream))
+            {
+                writer.Write("Hello World!");
+            }
+        }
+        using (FileStream fileStream = new FileStream("TEMPFILE.bin", FileMode.Create))
+        {
+            using (BinaryWriter writer = new BinaryWriter(fileStream))
+            {
+                writer.Write(10);
+                writer.Write(20.5); writer.Write("Hello World!");
+            }
+        }
+    }
+}
